8508c89b1ef77dc867e95ec72b226058f636e06f
paperlessformat <100112942+paperlessformat@users.noreply.github.com>
2022-04-13 21:09:44 -0700

    added angles bw 2 directions

---
+++ angle_between_two_atomic_bonds.m
@@ -0,0 +1,12 @@
+%%Compute angles between atomic bonds
+g = get_metric_tensor(a, b, c, alpha, beta, gamma); %Assume already have the function in another file
+
+s = [s1, s2, s3];
+t = [t1, t2, t3];
+
+s_length = sqrt(s*g*s');
+t_length = sqrt(t*g*t');
+
+theta = acosd(s*g*t'/(s_length*t_length));
+
+

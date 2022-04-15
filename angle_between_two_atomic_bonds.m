a2d51d348eb7e4633da29db6564dd666481bccbc
paperlessformat <100112942+paperlessformat@users.noreply.github.com>
2022-04-15 12:54:16 -0700

    added something

--- angle_between_two_atomic_bonds.m
+++ angle_between_two_atomic_bonds.m
@@ -1,6 +1,7 @@
 %%Compute angles between atomic bonds
 g = get_metric_tensor(a, b, c, alpha, beta, gamma); %Assume already have the function in another file
 
+%add your own vectors
 s = [s1, s2, s3];
 t = [t1, t2, t3];
 

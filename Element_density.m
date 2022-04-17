699b76c04d3ab5c18848b1abba6b6a4302cf8ee4
paperlessformat <100112942+paperlessformat@users.noreply.github.com>
2022-04-17 13:26:43 -0700

    Update Element_density.m

--- Element_density.m
+++ Element_density.m
@@ -11,12 +11,12 @@ gamma = 90.00000;
 
 %%Calculate Unit-Cell Volume
 a = a*(1*10^-8); %converting Amstrong to cm
-V = a^3;
+V = a^3; %the volume formula will be different if not CUB
 
 %%Calculate mass of the n atoms
 M = 100; %in unit of g/mol for that particular element
 
-m = M/(6.022*10^23) * n; %coverting g/mol to g/atoms with Avogadro's number, then multiple by n atoms, the final unit is grams
+m = M/(6.022*10^23) * n; %coverting g/mol to g/atoms with Avogadro's number, then multiple by n atoms, the final unit is in grams
 
 %%Calculate density of the element
 rho = m/V;
